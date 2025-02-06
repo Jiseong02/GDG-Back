0c43f10e5a4c69e449dc3735f1540b21da083f7c
Jiseong <pupajusang01@gmail.com>
2025-02-06 11:31:11 +0900

    Feat: Spring Initializr 필수 파일 추가

---
+++ src/main/java/com/gdg/gdgback/GdgbackApplication.java
@@ -0,0 +1,13 @@
+package com.gdg.gdgback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdgbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdgbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdg/gdgback/GdgbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdg.gdgback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdgbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
