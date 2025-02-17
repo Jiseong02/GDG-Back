5a15e7d7abf9eb9d9dfa8d9d0ca4860141595c64
PPmwaungmwaung <pupajusang01@gmail.com>
2025-02-18 00:41:46 +0900

    Refactor: Mapper 레이어 삭제

--- src/main/java/com/gdg/gdgback/DTO/Response/User/UserReadResponseDto.java
+++ src/main/java/com/gdg/gdgback/DTO/Response/User/UserReadResponseDto.java
@@ -3,9 +3,12 @@ package com.gdg.gdgback.DTO.Response.User;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Date;
+
 @Builder
 @Data
 public class UserReadResponseDto {
     private String id;
     private String name;
+    private Date date;
 }
--- src/main/java/com/gdg/gdgback/Service/Implement/UserServiceImpl.java
+++ src/main/java/com/gdg/gdgback/Service/Implement/UserServiceImpl.java
@@ -5,12 +5,12 @@ import com.gdg.gdgback.DTO.Request.User.UserReadRequestDto;
 import com.gdg.gdgback.DTO.Response.User.UserReadListResponseDto;
 import com.gdg.gdgback.DTO.Response.User.UserReadResponseDto;
 import com.gdg.gdgback.Document.UserDocument;
-import com.gdg.gdgback.Mapper.UserMapper;
 import com.gdg.gdgback.Repository.UserRepository;
 import com.gdg.gdgback.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -22,17 +22,36 @@ public class UserServiceImpl implements UserService {
         if(userRepository.findById(userCreateRequestDto.getId()).isPresent()) {
             throw new IllegalArgumentException("이미 존재하는 사용자입니다.");
         }
-        userRepository.save(UserMapper.toUserDocument(userCreateRequestDto));
+        userRepository.save(
+                UserDocument.builder()
+                        .id(userCreateRequestDto.getId())
+                        .name(userCreateRequestDto.getName())
+                        .build()
+        );
     }
 
     public UserReadResponseDto readUser(UserReadRequestDto readRequestDto) {
         UserDocument userDocument = userRepository.findById(readRequestDto.getId())
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
-        return UserMapper.toUserReadResponseDto(userDocument);
+        return UserReadResponseDto.builder()
+                .id(userDocument.getId())
+                .name(userDocument.getName())
+                .date(userDocument.getDate())
+                .build();
     }
 
     public UserReadListResponseDto readUserList() {
         List<UserDocument> userDocumentList = userRepository.findAll();
-        return UserMapper.toUserReadListResponseDto(userDocumentList);
+        ArrayList<UserReadResponseDto> users = new ArrayList<>();
+        for(UserDocument userDocument : userDocumentList) {
+            users.add(
+                    UserReadResponseDto.builder()
+                            .id(userDocument.getId())
+                            .name(userDocument.getName())
+                            .date(userDocument.getDate())
+                            .build()
+            );
+        }
+        return UserReadListResponseDto.builder().users(users).build();
     }
 }
