071c30d54aca3f53e14fa016e40dd87fe1c014e6
Jiseong02 <pupajusang01@gmail.com>
2025-03-13 19:01:50 +0900

    Fix: 입력 오류에 대한 안내 메시지가 출력되지 않던 버그 해결

--- src/main/java/com/gdg/gdgback/Global/ExceptionController.java
+++ src/main/java/com/gdg/gdgback/Global/ExceptionController.java
@@ -1,10 +1,13 @@
 package com.gdg.gdgback.Global;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ExceptionController {
@@ -15,5 +18,14 @@ public class ExceptionController {
     @ExceptionHandler
     public ResponseEntity<String> handleIOException(IOException e) {
         return ResponseEntity.badRequest().body("입력 오류 발생: " + e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+        return ResponseEntity.badRequest().body(errors);
     }
 }
