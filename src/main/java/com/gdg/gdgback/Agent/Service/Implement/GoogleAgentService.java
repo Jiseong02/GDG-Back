4009d19f763fbb6c58b9621d909d6129e21ed5cc
Jiseong02 <pupajusang01@gmail.com>
2025-03-14 19:43:30 +0900

    Feat: 시스템 프롬프트 내용 추가

--- src/main/java/com/gdg/gdgback/Agent/Service/Implement/GoogleAgentService.java
+++ src/main/java/com/gdg/gdgback/Agent/Service/Implement/GoogleAgentService.java
@@ -39,7 +39,15 @@ public class GoogleAgentService implements AgentService {
                         .build()
         );
 
-        String response = model.generateResponse(textRequestDto.getContent());
+        String prompt = """
+                역할: 당신은 유능한 공황장애 전문 상담가로서 대답해야 합니다.
+                제한사항:
+                - 자신은 전문가가 아니니 다른 전문가에게 도움을 받아보라는 말은 환자에게 큰 절망감을 줄 것이므로 해선 안됩니다.
+                - 당신의 대답은 음성으로 변환될 것입니다. 그러므로, 이모티콘이나 특수 문자 등을 사용해선 안됩니다.
+                - 환자와 대화를 주고 받기 위해, 간결하게 대답해야 합니다.
+                프롬프트:
+                """ + textRequestDto.getContent();
+        String response = model.generateResponse(prompt);
         messageRepository.save(
                 MessageDocument.builder()
                         .counselId(textRequestDto.getCounselId())
