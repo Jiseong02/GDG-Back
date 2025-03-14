7d0524b13c2b71374e6d49c20b88e7a3dbe28e89
Jiseong02 <pupajusang01@gmail.com>
2025-03-14 15:48:04 +0900

    Feat: 사용자 아이디를 통한 일지 조회 기능

--- src/main/java/com/gdg/gdgback/Diary/DiaryMapper.java
+++ src/main/java/com/gdg/gdgback/Diary/DiaryMapper.java
@@ -1,14 +1,8 @@
 package com.gdg.gdgback.Diary;
 
-import com.gdg.gdgback.Counsel.CounselDocument;
-import com.gdg.gdgback.Counsel.CounselMapper;
 import com.gdg.gdgback.Counsel.DTO.Response.CounselReadResponseDto;
 import com.gdg.gdgback.Diary.DTO.Request.DiaryCreateRequestDto;
-import com.gdg.gdgback.Diary.DTO.Response.DiaryReadListResponseDto;
 import com.gdg.gdgback.Diary.DTO.Response.DiaryReadResponseDto;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class DiaryMapper {
     public static DiaryDocument dtoToDocument(DiaryCreateRequestDto createRequestDto) {
@@ -23,27 +17,17 @@ public class DiaryMapper {
                 .build();
     }
 
-    public static DiaryReadResponseDto documentToReadResponseDto(DiaryDocument diaryDocument) {
+    public static DiaryReadResponseDto documentToReadResponseDto(DiaryDocument diaryDocument, CounselReadResponseDto counsel) {
         return DiaryReadResponseDto.builder()
                 .id(diaryDocument.getId())
                 .userId(diaryDocument.getUserId())
+                .counsel(null)
                 .date(diaryDocument.getDate())
                 .picture(diaryDocument.getPicture())
                 .category(diaryDocument.getCategory())
                 .score(diaryDocument.getScore())
                 .title(diaryDocument.getTitle())
                 .content(diaryDocument.getContent())
-                .build();
-    }
-
-    public static DiaryReadListResponseDto documentToReadResponseDto(List<DiaryDocument> diaryDocumentList) {
-        ArrayList<DiaryReadResponseDto> diaryList = new ArrayList<>();
-        for(DiaryDocument diaryDocument: diaryDocumentList) {
-            diaryList.add(documentToReadResponseDto(diaryDocument));
-        }
-
-        return DiaryReadListResponseDto.builder()
-                .diaries(diaryList)
                 .build();
     }
 }
--- src/main/java/com/gdg/gdgback/Diary/DiaryService.java
+++ src/main/java/com/gdg/gdgback/Diary/DiaryService.java
@@ -6,7 +6,6 @@ import com.gdg.gdgback.Diary.DTO.Request.DiaryCreateRequestDto;
 import com.gdg.gdgback.Diary.DTO.Request.DiaryDeleteRequestDto;
 import com.gdg.gdgback.Diary.DTO.Response.DiaryReadListResponseDto;
 import com.gdg.gdgback.Diary.DTO.Response.DiaryReadResponseDto;
-import com.gdg.gdgback.User.UserService;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -14,12 +13,10 @@ import java.util.List;
 
 @Service
 public class DiaryService{
-    private final UserService userService;
     private final CounselRepository counselRepository;
     private final DiaryRepository diaryRepository;
 
-    DiaryService(UserService userService, CounselRepository counselRepository, DiaryRepository diaryRepository) {
-        this.userService = userService;
+    DiaryService(CounselRepository counselRepository, DiaryRepository diaryRepository) {
         this.counselRepository = counselRepository;
         this.diaryRepository = diaryRepository;
     }
@@ -29,45 +26,40 @@ public class DiaryService{
     }
 
     public DiaryReadResponseDto readDiary(String id) {
-        // 일지 조회
         DiaryDocument diaryDocument = diaryRepository.findById(id)
                 .orElseThrow(DiaryNotFoundException::new);
-        DiaryReadResponseDto responseDto = DiaryMapper.documentToReadResponseDto(diaryDocument);
-
-        // 상담 ID를 통해 조회한 상담 대입
-        responseDto.setCounsel(
-                counselRepository.findById(diaryDocument.getCounselId())
-                        .map(CounselMapper::documentToDto)
-                        .orElse(null)
-        );
-
-        return responseDto;
+        return convertDocumentToDto(diaryDocument);
     }
 
     public DiaryReadListResponseDto readDiaryList() {
         List<DiaryDocument> diaryDocumentList = diaryRepository.findAll();
-
-        ArrayList<DiaryReadResponseDto> diaryReadResponseDtoList = documentListToReadResponseDtoList(diaryDocumentList);
-        return DiaryReadListResponseDto.builder()
-                .diaries(diaryReadResponseDtoList)
-                .build();
+        return convertDocumentListToListDto(diaryDocumentList);
     }
 
     public DiaryReadListResponseDto readDiaryListByUserId(String id) {
         List<DiaryDocument> diaryDocumentList = diaryRepository.findAllByUserId(id);
-
-        ArrayList<DiaryReadResponseDto> diaryReadResponseDtoList = documentListToReadResponseDtoList(diaryDocumentList);
-        return DiaryReadListResponseDto.builder()
-                .diaries(diaryReadResponseDtoList)
-                .build();
+        return convertDocumentListToListDto(diaryDocumentList);
     }
 
     public void deleteDiary(DiaryDeleteRequestDto deleteRequestDto) throws DiaryNotFoundException {
         DiaryDocument diaryDocument = diaryRepository.findById(deleteRequestDto.getId())
                 .orElseThrow(DiaryNotFoundException::new);
-
         diaryRepository.delete(diaryDocument);
     }
 
-    private DiaryReadResponseDto
+    private DiaryReadResponseDto convertDocumentToDto(DiaryDocument document) {
+        CounselReadResponseDto counsel = counselRepository.findById(document.getCounselId())
+                .map(CounselMapper::documentToDto)
+                .orElse(null);
+        return DiaryMapper.documentToReadResponseDto(document, counsel);
+    }
+
+    private DiaryReadListResponseDto convertDocumentListToListDto(List<DiaryDocument> documentList) {
+        ArrayList<DiaryReadResponseDto> dtoArrayList = new ArrayList<>();
+        for(DiaryDocument document : documentList) {
+            DiaryReadResponseDto dto = convertDocumentToDto(document);
+            dtoArrayList.add(dto);
+        }
+        return DiaryReadListResponseDto.builder().diaries(dtoArrayList).build();
+    }
 }
