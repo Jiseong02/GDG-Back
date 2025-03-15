064adb11f1fff46517463f1490fb50c41bab8121
Jiseong02 <pupajusang01@gmail.com>
2025-03-15 14:17:05 +0900

    Feat: CORS 전체 허용

--- src/main/java/com/gdg/gdgback/Global/WebConfig.java
+++ src/main/java/com/gdg/gdgback/Global/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("http://localhost:51014") // 특정 Origin 허용
+                .allowedOriginPatterns("*") // 특정 Origin 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드 지정
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키 및 인증정보 포함 허용
