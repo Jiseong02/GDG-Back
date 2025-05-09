5ff3ee23e2b11136c7d83996e408d797f31ac0af
PPmwaungmwaung <pupajusang01@gmail.com>
2025-05-10 07:44:39 +0900

    Fix: 다이어리 이미지 URL 경로 수정

--- src/main/java/com/gdg/gdgback/Global/WebConfig.java
+++ src/main/java/com/gdg/gdgback/Global/WebConfig.java
@@ -2,6 +2,7 @@ package com.gdg.gdgback.Global;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,5 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드 지정
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키 및 인증정보 포함 허용
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:/app/images/");
     }
 }
