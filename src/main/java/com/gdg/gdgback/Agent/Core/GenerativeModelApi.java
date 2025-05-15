75c8159d1189901b7f4575d1c2a669737837e0fb
Jiseong02 <pupajusang01@gmail.com>
2025-05-15 11:43:30 +0900

    Fix: 에이전트 DANGEROUS_CONTENT 제약 제거

--- src/main/java/com/gdg/gdgback/Agent/Core/GenerativeModelApi.java
+++ src/main/java/com/gdg/gdgback/Agent/Core/GenerativeModelApi.java
@@ -2,6 +2,8 @@ package com.gdg.gdgback.Agent.Core;
 
 import com.gdg.gdgback.Agent.Exception.AgentFailedToRespondException;
 import com.google.cloud.vertexai.VertexAI;
+import com.google.cloud.vertexai.api.HarmCategory;
+import com.google.cloud.vertexai.api.SafetySetting;
 import com.google.cloud.vertexai.generativeai.GenerativeModel;
 import com.google.cloud.vertexai.generativeai.ResponseHandler;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
 @Profile("!test")
@@ -20,6 +23,11 @@ public class GenerativeModelApi {
     GenerativeModelApi() {
         String projectId = "fair-backbone-449407-u7";
         this.generativeModel = new GenerativeModel("gemini-1.5-flash-001", new VertexAI(projectId, "asia-northeast3"));
+        this.generativeModel.withSafetySettings(Collections.singletonList(
+                SafetySetting.newBuilder()
+                        .setCategory(HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT)
+                        .setThreshold(SafetySetting.HarmBlockThreshold.BLOCK_NONE).build())
+        );
         this.generativeModelInJapan = new GenerativeModel("gemini-1.5-flash-001", new VertexAI(projectId, "asia-northeast1"));
     }
 
